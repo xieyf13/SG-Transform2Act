8bb86e988c5a9bf64c4bc9cb35ead39142ca58d9
alpc91 <459388027@qq.com>
2024-09-26 17:38:42 +0800

    evoant_sumo

--- test_sumo.py
+++ test_sumo.py
@@ -2,6 +2,6 @@ import os
 import subprocess
 
 
-for ckpt in range(1, 200):
-    command = f"python display.py --cfg config/test-robo-sumo-sgdevspider-devspider-v0.yaml --ckpt {ckpt} --ckpt_dir tmp/models"
+for ckpt in range(1, 130):
+    command = f"python display.py --cfg config/test-robo-sumo-sgant-ant-v0.yaml --ckpt {ckpt} --ckpt_dir tmp/models_ants"
     os.system(command)
