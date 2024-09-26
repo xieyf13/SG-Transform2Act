9b3c5c5e79654e651cf1ab48b70dffb6c472bb3b
alpc91 <459388027@qq.com>
2024-09-26 22:11:32 +0800

    sumo

--- test_sumo.py
+++ test_sumo.py
@@ -3,5 +3,5 @@ import subprocess
 
 
 for ckpt in range(1, 130):
-    command = f"python display.py --cfg config/test-robo-sumo-sgant-ant-v0.yaml --ckpt {ckpt} --ckpt_dir tmp/models_ants"
+    command = f"python display.py --cfg config/SG/test-robo-sumo-sgdevant-ant-v0.yaml --ckpt {ckpt} --ckpt_dir tmp/models_sgdevant_ant"
     os.system(command)
